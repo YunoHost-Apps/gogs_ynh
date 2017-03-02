b36c0265c822ca9fd78b4f11dd7ea92556ababe9
Moul <moul@moul.re>
2017-03-02 13:54:38 +0100

    [enh] upgrade to releases 0.10.1. Change version pattern.

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,17 +5,17 @@
 APPNAME="gogs"
 
 # Gogs version
-VERSION="0.9.141"
+VERSION="0.10.1"
 
 # Detect the system architecture to download the right tarball
 # NOTE: `uname -m` is more accurate and universal than `arch`
 # See https://en.wikipedia.org/wiki/Uname
 if [ -n "$(uname -m | grep 64)" ]; then
-	ARCHITECTURE="amd64"
+	ARCHITECTURE="linux_amd64"
 elif [ -n "$(uname -m | grep 86)" ]; then
-	ARCHITECTURE="386"
+	ARCHITECTURE="linux_386"
 elif [ -n "$(uname -m | grep arm)" ]; then
-	ARCHITECTURE="armv6"
+	ARCHITECTURE="raspi2_armv6"
 else
 	echo 'Unable to detect your achitecture, please open a bug describing \
         your hardware and the result of the command "uname -m".'
@@ -23,7 +23,7 @@ else
 fi
 
 # Remote URL to fetch Gogs tarball
-GOGS_BINARY_URL="https://github.com/gogits/gogs/releases/download/v${VERSION}/linux_${ARCHITECTURE}.zip"
+GOGS_BINARY_URL="https://github.com/gogits/gogs/releases/download/v${VERSION}/${ARCHITECTURE}.zip"
 
 #
 # Common helpers
