2400eee2b1a962d1a6ec32eabc2c58346ffd9782
ericgaspar <junk.eg@free.fr>
2020-11-20 09:28:52 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -120,38 +120,4 @@ ynh_check_starting () {
     local app_log="${2:-/var/log/$service_name/$service_name.log}"
     local timeout=${3:-300}
 
-    ynh_clean_check_starting () {
-        # Stop the execution of tail.
-        kill -s 15 $pid_tail 2>&1
-        ynh_secure_remove "$templog" 2>&1
-    }
-
-    echo "Starting of $service_name" >&2
-    systemctl stop $service_name
-    local templog="$(mktemp)"
-    # Following the starting of the app in its log
-    tail -F -n0 "$app_log" > "$templog" &
-    # Get the PID of the tail command
-    local pid_tail=$!
-    systemctl start $service_name
-
-    local i=0
-    for i in `seq 1 $timeout`
-    do
-        # Read the log until the sentence is found, that means the app finished to start. Or run until the timeout
-        if grep --quiet "$line_to_match" "$templog"
-        then
-            echo "The service $service_name has correctly started." >&2
-            break
-        fi
-        echo -n "." >&2
-        sleep 1
-    done
-    if [ $i -eq $timeout ]
-    then
-        echo "The service $service_name didn't fully started before the timeout." >&2
-    fi
-
-    echo ""
-    ynh_clean_check_starting
-}
+   
