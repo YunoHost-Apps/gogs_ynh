8134c875c897ae289639fa730fb5458de380d8cf
ericgaspar <junk.eg@free.fr>
2020-11-20 11:22:58 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,11 +2,6 @@
 # SET ALL CONSTANTS
 #=================================================
 
-
-DATADIR="/home/$app"
-REPO_PATH="$DATADIR/repositories"
-DATA_PATH="$DATADIR/data"
-
 # Detect the system architecture to download the right tarball
 # NOTE: `uname -m` is more accurate and universal than `arch`
 # See https://en.wikipedia.org/wiki/Uname
@@ -24,34 +19,6 @@ ynh_detect_arch(){
         fi
         echo $architecture
 }
-
-
-# Check the architecture
-#
-# example: architecture=$(ynh_detect_arch)
-#
-# usage: ynh_detect_arch
-#
-# Requires YunoHost version 2.2.4 or higher.
-# ynh_detect_arch(){
-#         local architecture
-#         if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
-#                 architecture="arm64"
-#         elif [ -n "$(uname -m | grep 86)" ]; then
-#                 architecture="i386"                
-#         elif [ -n "$(uname -m | grep 64)" ]; then
-#                 architecture="x86-64"
-#         elif [ -n "$(uname -m | grep armv7)" ]; then
-#                 architecture="armv7"
-#         elif [ -n "$(uname -m | grep armv6)" ]; then
-#                 architecture="armv6"
-#         elif [ -n "$(uname -m | grep armv5)" ]; then
-#                 architecture="armv5"
-#         else
-#                 architecture="unknown"
-#         fi
-#         echo $architecture
-# }
 
 #=================================================
 # DEFINE ALL COMMON FONCTIONS
