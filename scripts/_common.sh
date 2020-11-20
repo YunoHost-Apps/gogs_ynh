715e6ee32b5376f59a70ac563903be9beebab469
ericgaspar <junk.eg@free.fr>
2020-11-20 10:09:41 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,16 +10,47 @@ DATA_PATH="$DATADIR/data"
 # Detect the system architecture to download the right tarball
 # NOTE: `uname -m` is more accurate and universal than `arch`
 # See https://en.wikipedia.org/wiki/Uname
+ynh_detect_arch(){
+        local architecture
 if [ -n "$(uname -m | grep 64)" ]; then
 	architecture="x86-64"
 elif [ -n "$(uname -m | grep 86)" ]; then
 	architecture="i386"
 elif [ -n "$(uname -m | grep arm)" ]; then
-	architecture="arm"
+	architecture="armv7"
 else
 	ynh_die "Unable to detect your achitecture, please open a bug describing \
         your hardware and the result of the command \"uname -m\"." 1
 fi
+}
+
+
+# Check the architecture
+#
+# example: architecture=$(ynh_detect_arch)
+#
+# usage: ynh_detect_arch
+#
+# Requires YunoHost version 2.2.4 or higher.
+# ynh_detect_arch(){
+#         local architecture
+#         if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+#                 architecture="arm64"
+#         elif [ -n "$(uname -m | grep 86)" ]; then
+#                 architecture="i386"                
+#         elif [ -n "$(uname -m | grep 64)" ]; then
+#                 architecture="x86-64"
+#         elif [ -n "$(uname -m | grep armv7)" ]; then
+#                 architecture="armv7"
+#         elif [ -n "$(uname -m | grep armv6)" ]; then
+#                 architecture="armv6"
+#         elif [ -n "$(uname -m | grep armv5)" ]; then
+#                 architecture="armv5"
+#         else
+#                 architecture="unknown"
+#         fi
+#         echo $architecture
+# }
 
 #=================================================
 # DEFINE ALL COMMON FONCTIONS
