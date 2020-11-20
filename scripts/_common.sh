b8ff995b62a0ef8f61cd671df7c12c669abf02cd
ericgaspar <junk.eg@free.fr>
2020-11-20 10:22:05 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,16 +12,17 @@ DATA_PATH="$DATADIR/data"
 # See https://en.wikipedia.org/wiki/Uname
 ynh_detect_arch(){
         local architecture
-if [ -n "$(uname -m | grep 64)" ]; then
-	architecture="x86-64"
-elif [ -n "$(uname -m | grep 86)" ]; then
-	architecture="i386"
-elif [ -n "$(uname -m | grep arm)" ]; then
-	architecture="armv7"
-else
-	ynh_die "Unable to detect your achitecture, please open a bug describing \
-        your hardware and the result of the command \"uname -m\"." 1
-fi
+        if [ -n "$(uname -m | grep 64)" ]; then
+	       architecture="x86-64"
+        elif [ -n "$(uname -m | grep 86)" ]; then
+	       architecture="i386"
+        elif [ -n "$(uname -m | grep arm)" ]; then
+	       architecture="armv7"
+        else
+	       ynh_die "Unable to detect your achitecture, please open a bug describing \
+            your hardware and the result of the command \"uname -m\"." 1
+    fi
+    echo $architecture
 }
 
 
